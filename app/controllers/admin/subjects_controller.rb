f2e23719bf293534a1c7fb505e86c8e189cfb000
tooyosi <oluwatoyosi.oyegoke@gmail.com>
2026-07-02 05:22:11 -0500

    remove predictions from subject view/list

--- app/controllers/admin/subjects_controller.rb
+++ app/controllers/admin/subjects_controller.rb
@@ -3,13 +3,13 @@
 module Admin
   class SubjectsController < BaseController
     def index
-      scope = Subject.includes(:context, :reductions, :predictions).order(id: :desc)
+      scope = Subject.includes(:context, :reductions).order(id: :desc)
       scope = scope.where(zooniverse_subject_id: params[:zooniverse_subject_id]) if params[:zooniverse_subject_id].present?
       @subjects, @pagination = paginate(scope)
     end
 
     def show
-      @subject = Subject.includes(:context, :reductions, :predictions).find(params[:id])
+      @subject = Subject.includes(:context, :reductions).find(params[:id])
     end
   end
 end
